c7e3bd5cf99913e4961a6a809b75bddc7d0bf6c7
JordanRoy-Lemay <roy-lemay.jordan@carrefour.cegepvicto.ca>
2016-11-17 15:19:26 -0500

    Mouvement/Sound
    Les mouvements ont été ajouté, la hitbox de la flèche ainsi que la
    musique et les sons.

--- ExerciceJeu/Exercice01/Game1.cs
+++ ExerciceJeu/Exercice01/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+
 
 namespace Exercice01
 {
@@ -11,6 +14,14 @@ namespace Exercice01
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Rectangle fenetre;
+        GameObject heros;
+        GameObject badboy;
+        GameObject arrow;
+        SoundEffect son;
+        SoundEffectInstance dying;
+        SoundEffectInstance musique;
+        
 
         public Game1()
         {
@@ -27,7 +38,11 @@ namespace Exercice01
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            this.graphics.PreferredBackBufferWidth = graphics.GraphicsDevice.DisplayMode.Width;
+            this.graphics.PreferredBackBufferHeight = graphics.GraphicsDevice.DisplayMode.Height;
 
+
+            this.graphics.ToggleFullScreen();
             base.Initialize();
         }
 
@@ -39,9 +54,42 @@ namespace Exercice01
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            fenetre = graphics.GraphicsDevice.Viewport.Bounds;
+            fenetre.Width = graphics.GraphicsDevice.DisplayMode.Width;
+            fenetre.Height = graphics.GraphicsDevice.DisplayMode.Height;
+
+            Song song = Content.Load<Song>("LinkMusic");
+            MediaPlayer.Play(song);
+            son = Content.Load<SoundEffect>("LTTP_Link_Dying");
+            dying = son.CreateInstance();
+
+            heros = new GameObject();
+            heros.estVivant = true;
+            heros.vitesse = 10;
+            heros.sprite = Content.Load<Texture2D>("LinkGood.png");
+            heros.position = heros.sprite.Bounds;
+            heros.position.Offset(650, 300);
+
+            UpdateHeros();
+
+            badboy = new GameObject();
+            badboy.estVivant = true;
+            badboy.vitesse = 1;
+            badboy.sprite = Content.Load<Texture2D>("LinkBad.png");
+            badboy.position = badboy.sprite.Bounds;
+            badboy.position.Offset(0,0);
+
+            arrow = new GameObject();
+            arrow.estVivant = true;
+            arrow.vitesse = 25;
+            arrow.sprite = Content.Load<Texture2D>("ArrowMonogame.png");
+            arrow.position = arrow.sprite.Bounds;
 
             // TODO: use this.Content to load your game content here
         }
+
+        
+
 
         /// <summary>
         /// UnloadContent will be called once per game and is the place to unload
@@ -63,8 +111,82 @@ namespace Exercice01
                 Exit();
 
             // TODO: Add your update logic here
+            //DÉPLACEMENT DU HERO
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                heros.position.X += heros.vitesse;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                heros.position.X -= heros.vitesse;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                heros.position.Y -= heros.vitesse;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                heros.position.Y += heros.vitesse;
+            }
+            //DÉPLACEMENT DU BADBOY
+            if (badboy.position.X == 0)
+            {
+                badboy.vitesse = 10;
+            }
+            if (badboy.position.X >= fenetre.Right - badboy.sprite.Width)
+            {
+                badboy.vitesse = -10;
+            }
+            badboy.position.X += badboy.vitesse;
+
+            //DÉPLACEMENT ET RESPAWN ARROW
+            arrow.position.Y += arrow.vitesse;
+
+            if (arrow.position.Y > fenetre.Bottom - arrow.sprite.Height)
+            {
+                arrow.position.X = badboy.position.X;
+                arrow.position.Y = badboy.position.Y;
+                arrow.position.Y += arrow.vitesse;
+            }
+            //MORT DU HERO
+            if (heros.position.Intersects(arrow.position))
+            {
+                heros.estVivant = false;
+                dying.Play();
+            }
+            if (heros.position.Intersects(badboy.position))
+            {
+                heros.estVivant = false;
+                dying.Play();
+            }
+
+            UpdateHeros();
 
             base.Update(gameTime);
+        }
+        
+        protected void UpdateHeros()
+        {
+            //EMPECHE QUE LE HERO SORTE
+            if (heros.position.X < fenetre.Left)
+            {
+                heros.position.X = fenetre.Left;
+            }
+            if (heros.position.X+ heros.sprite.Bounds.Width > fenetre.Right)
+            {
+                heros.position.X = fenetre.Right - heros.sprite.Bounds.Width;
+            }
+            if (heros.position.Y < fenetre.Top)
+            {
+                heros.position.Y = fenetre.Top;
+            }
+            if (heros.position.Y + heros.sprite.Bounds.Height > fenetre.Bottom)
+            {
+                heros.position.Y = fenetre.Bottom - heros.sprite.Bounds.Height;
+            }
+            
+
+
         }
 
         /// <summary>
@@ -76,6 +198,15 @@ namespace Exercice01
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+            if (heros.estVivant == true)
+            {
+                spriteBatch.Draw(heros.sprite, heros.position, Color.White);
+            }
+            spriteBatch.Draw(badboy.sprite, badboy.position, Color.White);
+            spriteBatch.Draw(arrow.sprite, arrow.position, Color.White);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
